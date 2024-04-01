94e5107640e47c89d2f69ec0ec17f02daf282c84
Romil Jain <40470068+Romilj012@users.noreply.github.com>
2024-03-31 21:45:35 -0400

    Time: 49 ms (22.02%) | Memory: 16.5 MB (83.58%) - LeetSync

---
+++ 38-count-and-say/count-and-say.py
@@ -0,0 +1,36 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        # s = "11"
+        # if n ==1 : 
+        #     return "1"
+        # if n ==2:
+        #     return "11"
+        # for i in range(2, n):
+        #     count = 1 
+        #     result = ""
+        #     for j in range(len(s)):
+        #         if j == len(s)-1:
+        #             result = result + str(count) + s[j] 
+        #         elif s[j] == s[j+1]:
+        #             count += 1
+        #         else:
+        #             result = result + str(count) + s[j]
+        #             count = 1
+        #     s = result
+        # return result
+        #recurssion
+        if n == 1:
+            return "1"
+        prev = self.countAndSay(n-1)
+        result = ""
+        count = 1
+        for i in range(len(prev)):
+            if i == len(prev)-1 or prev[i] != prev[i+1]:
+                result = result + str(count) + prev[i]
+                count =1
+            else:
+                count +=1
+        return result
+
+
+        
