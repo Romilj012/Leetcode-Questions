a247a2329157831744519f0dab816b734ed6eb46
Romil Jain <40470068+Romilj012@users.noreply.github.com>
2024-07-10 23:14:45 -0400

    Time: 116 ms (14.15%) | Memory: 23.1 MB (90.22%) - LeetSync

---
+++ 338-counting-bits/counting-bits.py
@@ -0,0 +1,28 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        li = [0]
+        def countBit(i):
+            c = 0
+            while i:
+                if i&1:
+                    c+=1
+                i = i>>1
+            return c
+        for i in range(1,n+1):
+            a = countBit(i)
+            li.append(a)
+        return li
+
+# class Solution:
+#     def countBits(self, n: int) -> List[int]:
+#         def countBit(i):
+#             c = 0
+#             while i:
+#                 if i & 1:
+#                     c += 1
+#                 i = i >> 1
+#             return c
+
+#         li = [countBit(i) for i in range(n + 1)]
+#         return li
+        
