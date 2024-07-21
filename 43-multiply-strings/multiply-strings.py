edf69914413d1bbe1beaabcfc6ea1d856e1aa790
Romil Jain <40470068+Romilj012@users.noreply.github.com>
2024-07-21 18:19:36 -0400

    Time: 101 ms (25.86%) | Memory: 16.6 MB (40.49%) - LeetSync

--- 43-multiply-strings/multiply-strings.py
+++ 43-multiply-strings/multiply-strings.py
@@ -1,21 +1,17 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        if num1 == '0' or num2 == '0':
+        if '0' in [num1, num2]:
             return '0'
-        
-        def decode(num):
-            ans = 0
-            for i in num:
-                ans = ans*10 +(ord(i) - ord('0'))
-            return ans
-
-        def encode(s):
-            news = ''
-            while s:
-                a = s % 10
-                s = s // 10
-                news = chr(ord('0') + a) + news
-            return news
-        
-        return encode(decode(num1)*decode(num2))
-        
+        res = [0]*(len(num1) + len(num2))
+        num1, num2 = num1[::-1], num2[::-1]
+        for i in range(len(num1)):
+            for j in range(len(num2)):
+                d = int(num1[i]) * int (num2[j])
+                res[i + j] += d
+                res[i+j+1] += res[i+j] // 10
+                res[i+j] = res[i+j] % 10
+        res, beg = res[::-1], 0
+        while beg < len(res) and res[beg] == 0:
+            beg += 1
+        res = map(str, res[beg:])
+        return "".join(res)
