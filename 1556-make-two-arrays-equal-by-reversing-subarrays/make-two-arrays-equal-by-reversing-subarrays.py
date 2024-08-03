df4a4871a53231197cc6a427b367e846fa5d50ab
Romil Jain <40470068+Romilj012@users.noreply.github.com>
2024-08-03 12:55:25 -0500

    Time: 78 ms (33.83%) | Memory: 16.7 MB (59.96%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.py
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.py
@@ -2,21 +2,20 @@ class Solution:
     def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
         if len(target) != len(arr):
             return False
-        
-        count = {}
-        
+
+        target_count = {}
+        arr_count = {}
+
         for num in target:
-            if num in count:
-                count[num] += 1
+            if num in target_count:
+                target_count[num] += 1
             else:
-                count[num] = 1
-        
+                target_count[num] = 1
+
         for num in arr:
-            if num in count:
-                count[num] -= 1
-                if count[num] == 0:
-                    del count[num]
+            if num in arr_count:
+                arr_count[num] += 1
             else:
-                return False
-        
-        return not count
+                arr_count[num] = 1
+
+        return target_count == arr_count
