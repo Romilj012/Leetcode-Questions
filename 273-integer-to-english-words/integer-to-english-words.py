0ea2ae3dae58428538c7b171b12732f68539ff46
Romil Jain <40470068+Romilj012@users.noreply.github.com>
2024-08-07 11:50:58 -0500

    Time: 49 ms (6.63%) | Memory: 16.6 MB (47.47%) - LeetSync

--- 273-integer-to-english-words/integer-to-english-words.py
+++ 273-integer-to-english-words/integer-to-english-words.py
@@ -3,7 +3,6 @@ class Solution:
         if num == 0:
             return "Zero"
         
-        # Dictionaries to map numbers to words
         below_20 = {
             0: 'Zero', 1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', 6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine', 
             10: 'Ten', 11: 'Eleven', 12: 'Twelve', 13: 'Thirteen', 14: 'Fourteen', 15: 'Fifteen', 16: 'Sixteen', 
