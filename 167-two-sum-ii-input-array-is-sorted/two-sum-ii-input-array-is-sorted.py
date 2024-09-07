540535ee76f31308596c8d4a8470a86cf4a856bc
Romil Jain <40470068+Romilj012@users.noreply.github.com>
2024-09-07 11:48:25 -0500

    Time: 110 ms (25.60%) | Memory: 17.7 MB (42.10%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,8 +1,20 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        hashtable = dict()
-        for i, num in enumerate(numbers):
-            if target - num in hashtable:
-                return [hashtable[target - num]+1, i+1]
-            hashtable[numbers[i]] = i
-        return []
+        # hashtable = dict()
+        # for i, num in enumerate(numbers):
+        #     if target - num in hashtable:
+        #         return [hashtable[target - num]+1, i+1]
+        #     hashtable[numbers[i]] = i
+        # return []
+        l, r = 0, len(numbers)-1
+        res = []
+        for n in numbers:
+            if numbers[l]+ numbers[r] < target:
+                l = l+1
+            if numbers[l] + numbers[r] > target:
+                r = r-1
+            if numbers[l]+ numbers[r] == target:
+                res.extend([l+1, r+1])
+                break
+        return res
+
