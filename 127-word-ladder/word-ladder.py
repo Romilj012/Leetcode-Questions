3085c0303da460eb6f4144b26aa6c98b703f95b9
Romil Jain <40470068+Romilj012@users.noreply.github.com>
2024-09-21 11:45:43 -0500

    Time: 110 ms (91.99%) | Memory: 19.7 MB (39.53%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -26,3 +26,30 @@ class Solution:
         return 0
 
         
+
+# class Solution:
+# 	def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+		# if endWord not in wordList:
+		# 	return 0
+		# nei = collections.defaultdict(list)
+		# wordList.append(beginWord)
+		# for word in wordList:
+		# 	for j in range(len(word)):
+		# 		pattern = word[:j] + "*" + word[j+1:]
+		# 		nei[pattern].append(word)
+		# visit = set([beginWord])
+		# que = deque([beginWord])
+		# res = 1
+		# while que:
+		# 	for i in range(len(que)):
+		# 		word = que.popleft()
+		# 		if word == endWord:
+		# 			return res
+		# 		for j in range(len(word)):
+		# 			pattern = word[:j] + "*" + word[j+1:]
+		# 			for neiW in nei[pattern]:
+		# 					visit.add(neiW)
+		# 					que.append(neiW)
+		# 	res += 1
+		# return 0			
+        
