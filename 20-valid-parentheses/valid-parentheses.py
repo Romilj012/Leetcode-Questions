6c57b62bb3533d375c85b85bb28b008d1be51a7b
Romil Jain <40470068+Romilj012@users.noreply.github.com>
2024-10-09 22:47:49 -0500

    Time: 45 ms (5.99%) | Memory: 16.5 MB (58.54%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,19 +1,17 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        par = {"}":"{", "]":"[", ")":"("}
         stack = []
-        my_dict = {')': '(', ']': '[', '}': '{'}
         for i in s:
-            if i == "(" or i == "{" or  i == "[":
+            if i == "{" or i == "[" or i == "(":
                 stack.append(i)
-            if i == ")" or i == "}" or  i == "]":
-                if stack and my_dict[i] == stack[-1]:
+            if i == "}" or i == "]" or i == ")":
+                if stack and par[i] == stack[-1]:
                     stack.pop()
                 else:
                     return False
-        if not stack:
-            return True
-        return False
+        if stack:
+            return False
+        return True
 
-                    
-                
         
