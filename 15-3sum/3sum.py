0e344a8d07ac987b3f0ec3c9de45b247fc09f872
Romil Jain <40470068+Romilj012@users.noreply.github.com>
2024-12-13 10:51:30 -0600

    Time: 524 ms (77.67%) | Memory: 20.1 MB (36.47%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -18,4 +18,4 @@ class Solution:
                     while nums[l] == nums[l-1] and l < r:
                         l+=1
         return res
-                                
+                          
