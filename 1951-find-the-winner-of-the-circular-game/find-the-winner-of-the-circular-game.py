1ff621006964c7bfae6a6946e61db2e6effe5172
Romil Jain <40470068+Romilj012@users.noreply.github.com>
2024-12-13 12:22:52 -0600

    Time: 0 ms (100.00%) | Memory: 17.4 MB (13.04%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -1,11 +1,15 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        q = deque()
-        for i in range(1, n+1):
-            q.append(i)
-        while len(q) > 1:
-            q.rotate(-(k-1))
-            q.popleft()
-        return q[0]
+        # q = deque()
+        # for i in range(1, n+1):
+        #     q.append(i)
+        # while len(q) > 1:
+        #     q.rotate(-(k-1))
+        #     q.popleft()
+        # return q[0]
+        winner = 0  # Base case for n = 1 (0-based index)
+        for i in range(2, n + 1):  # Solve for 2 to n people
+            winner = (winner + k) % i
+        return winner + 1  # Convert to 1-based index
 
 
