ac03d6872dd978d4c8dc7b65f9100dd2bc92222b
Romil Jain <40470068+Romilj012@users.noreply.github.com>
2025-02-09 11:16:19 -0600

    Time: 599 ms (68.54%) | Memory: 0B (100.00%) - LeetSync

--- 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 SELECT e1.name as Employee
-FROM Employee e1 CROSS JOIN Employee e2
+FROM Employee e1 JOIN Employee e2
 ON e1.managerId = e2.id WHERE e1.salary > e2.salary;
