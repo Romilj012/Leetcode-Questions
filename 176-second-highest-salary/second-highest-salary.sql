9b33701c1004c7e8c6fbed4ec4464191af018f74
Romil Jain <40470068+Romilj012@users.noreply.github.com>
2025-02-10 21:54:51 -0600

    Time: 417 ms (72.43%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
-SELECT COALESCE(
-    (SELECT DISTINCT(salary)
-    FROM Employee
-    ORDER by salary desc
-    LIMIT 1 OFFSET 1), NULL) AS SecondHighestSalary;
+select
+(select distinct Salary 
+from Employee order by salary desc 
+limit 1 offset 1) 
+as SecondHighestSalary;
